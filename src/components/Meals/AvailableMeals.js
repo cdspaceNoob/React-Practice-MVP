b3260c3d498350ad264cf6f171c280db14fc08fc
voyager <cdspacenoob@gmail.com>
2023-02-13 13:18:38 +0900

    fetch data and generate Array

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,56 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-c3d53-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+      // 이 상태에서 responseData는 객체다. meals라는 걸 객체로 받아온 거다.
+      /*
+      {meals:
+        m1: {
+          name: "Shushi",
+          description: "Finest fish and veggies",
+          price: 22.99,
+        },
+        m2: {
+          name: "Schnitzel",
+          description: "A german specialty!",
+          price: 16.5,
+        },
+        m3: {
+          name: "Barbecue Burger",
+          description: "American, raw, meaty",
+          price: 12.99,
+        },
+        ...
+      }
+      */
+      // 배열로 변환하는 과정을 거친다.
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []); // 앱이 실행될 때 한 번만 가져와도 되는 상황이다.
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
